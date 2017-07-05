24fc62d74359c6511aee67fef64739f6aa55f7cd
Evan Chen <evan@Evans-MacBook-Pro.local>
2017-07-05 00:10:41 -0700

    add

--- BoatBoat/GameScene.swift
+++ BoatBoat/GameScene.swift
@@ -91,10 +91,10 @@ class GameScene : SCNScene{
     
     //moves boat forward based on next world z position
     func moveMasterBoat(){
-        //new vector for next position
-        let vectorNew = getZForward(node: masterBoat.presentation)
-        //applying new vector force
-        masterBoat.physicsBody?.applyForce(SCNVector3(-vectorNew.x*0.1,0,-vectorNew.z*0.1), asImpulse: true)
+            //new vector for next position
+            let vectorNew = getZForward(node: masterBoat.presentation)
+            //applying new vector force
+            masterBoat.physicsBody?.applyForce(SCNVector3(-vectorNew.x*0.1,0,-vectorNew.z*0.1), asImpulse: true)
     }
     
     
@@ -105,14 +105,13 @@ class GameScene : SCNScene{
     
     //rotates the boat based on user touch
     func rotateBoat(increment: Float){
-        //rotation of master boat
-        
-        //singling out presentation position and reapplying after euler rotation
-        let position = masterBoat.presentation.position
-        
-        masterBoat.eulerAngles.y += (increment*0.01) //scaling down increment
-        
-        masterBoat.position = position
+            //rotation of master boat
+            //singling out and reapplying presentation positon after euler rotation
+            let position = masterBoat.presentation.position
+
+            masterBoat.eulerAngles.y += (increment*0.01) //scaling down increment
+
+            masterBoat.position = position
     }
     
     
@@ -132,7 +131,7 @@ class GameScene : SCNScene{
         
         //Loading in camera
         gameCamera = self.rootNode.childNode(withName: "camera", recursively: true)
-        
+    
         //coloring background
         self.background.contents = UIColor(colorLiteralRed: 171/255, green: 203/255, blue: 1, alpha: 1)
         
@@ -198,7 +197,7 @@ class GameScene : SCNScene{
     func animateTerrain(){
         for plane in terrainPlane{
             plane.runAction(SCNAction.repeatForever(SCNAction.sequence([SCNAction.moveBy(x: 5, y: 0, z: 0, duration: 3), SCNAction.moveBy(x: -5, y: 0, z: 0, duration: 5)])))
-            plane.runAction(SCNAction.repeatForever(SCNAction.sequence([SCNAction.moveBy(x: 0, y: 1.5, z: 0, duration: 3), SCNAction.moveBy(x: 0, y: -1.5, z: 0, duration: 5)])))
+             plane.runAction(SCNAction.repeatForever(SCNAction.sequence([SCNAction.moveBy(x: 0, y: 1.5, z: 0, duration: 3), SCNAction.moveBy(x: 0, y: -1.5, z: 0, duration: 5)])))
         }
     }
     func manageTerrain(){
